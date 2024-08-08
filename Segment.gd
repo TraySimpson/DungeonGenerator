21696ea1406d81d5710a18938a495fb23fd2d7f3
Tray Simpson <tdsimpson123@gmail.com>
2024-08-08 14:25:58 -0600

    Add starting room

---
+++ Segment.gd
@@ -0,0 +1,7 @@
+class_name Segment
+extends RefCounted
+
+var size: Vector2i
+var position: Vector2i
+var weight: int
+var connections := []
--- TileMap.gd
+++ TileMap.gd
@@ -1,20 +1,34 @@
 extends TileMap
 
-@export var map_width: int = 100
-@export var map_height: int = 100
+@export var map_width: int = 71
+@export var map_height: int = 40
 
 const LAYER = 0
 const TILESET_SOURCE = 0
 const BLACK: Vector2i = Vector2i(0, 0)
 const WHITE: Vector2i = Vector2i(1, 0)
 
+
+
 func _ready():
+	var rooms = create_graph()
+	render_tilemap(rooms)
+	
+
+func create_graph() -> Array[Segment]:
+	var room = Segment.new()
+	room.size = Vector2i(3, 3)
+	room.position = Vector2i(map_width / 2 - 1, map_height - 2)
+	return [ room ]
+
+func render_tilemap(rooms: Array[Segment]):
 	for x in range(map_width):
 		for y in range(map_height):
-			var color = WHITE if getColor(x, y) else BLACK 
-			set_cell(LAYER, Vector2i(x, y), TILESET_SOURCE, color)
-			
-func getColor(x, y) -> bool:
-	return x % 10 == 0 or y % 10 == 0
+			set_cell(LAYER, Vector2i(x, y), TILESET_SOURCE, BLACK)
+	for room in rooms:
+		var start = room.position - (room.size / 2)
+		for x in range(room.size.x):
+			for y in range(room.size.y):
+				set_cell(LAYER, Vector2i(start.x + x, start.y + y), TILESET_SOURCE, WHITE)
 	
 	
