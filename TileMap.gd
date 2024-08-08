43476f5a7050c5099c3978b3a78166b19f101044
Tray Simpson <tdsimpson123@gmail.com>
2024-08-08 17:27:08 -0600

    Better visibility tiles, plus start on cellular automata

--- TileMap.gd
+++ TileMap.gd
@@ -7,7 +7,8 @@ const LAYER = 0
 const TILESET_SOURCE = 0
 const BLACK: Vector2i = Vector2i(0, 0)
 const WHITE: Vector2i = Vector2i(1, 0)
-
+const BLUE: Vector2i = Vector2i(0, 1)
+const GREEN: Vector2i = Vector2i(1, 1)
 
 
 func _ready():
@@ -23,32 +24,13 @@ func create_graph() -> Array[Segment]:
 	entrance.position = Vector2i(map_width / 2 - 1, map_height - 2)
 	rooms.append(entrance)
 	# Connecting rooms
-	var connectingRoomCount = randi_range(1, 4)
-	for roomIndex in connectingRoomCount:
-		var room = Segment.new()
-		entrance.connections.append(room)
-		room.size = Vector2i(randi_range(2, 6), randi_range(2, 6))
-		rooms.append(room)
-	calculate_weight(entrance)
-	calculate_connected_positions(entrance)
+	var room = Segment.new()
+	entrance.connections.append(room)
+	room.size = Vector2i(randi_range(2, 6), randi_range(2, 6))
+	room.position = Vector2i( 25, 23)
+	rooms.append(room)
 	return rooms
 	
-func calculate_weight(segment: Segment) -> int:
-	if (segment.connections.is_empty()):
-		segment.weight = segment.get_room_weight()
-		return segment.weight
-	var weight = 0
-	for connection in segment.connections:
-		weight += calculate_weight(connection)
-	segment.weight = weight
-	return weight
-
-func calculate_connected_positions(segment: Segment):
-	for room in segment.connections:
-		var ratio = float(room.weight) / segment.weight
-		room.position = segment.position + Vector2i(
-			
-		)
 
 func render_tilemap(rooms: Array[Segment]):
 	for x in range(map_width):
@@ -59,5 +41,13 @@ func render_tilemap(rooms: Array[Segment]):
 		for x in range(room.size.x):
 			for y in range(room.size.y):
 				set_cell(LAYER, Vector2i(start.x + x, start.y + y), TILESET_SOURCE, WHITE)
-	
-	
+	var connectionZone = Segment.new()
+	connectionZone.size = Vector2i(10, 19)
+	connectionZone.position = Vector2i(32, 27)
+	var start = connectionZone.position - (connectionZone.size / 2)
+	for x in range(connectionZone.size.x):
+			for y in range(connectionZone.size.y):
+				set_cell(LAYER, Vector2i(start.x + x, start.y + y), TILESET_SOURCE, BLUE)
+
+func cellular_automata():
+	pass
